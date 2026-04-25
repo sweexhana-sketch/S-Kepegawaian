718cc8f8bb92d4eb76dee98cee9ca6de199e016e
hanasweex <sweexhana@gmail.com>
2026-04-25 12:18:31 +0900

    Fix debug endpoint to use ES Modules instead of require()

--- api/debug.ts
+++ api/debug.ts
@@ -1,7 +1,11 @@
-export default function handler(req, res) {
-  const fs = require('fs');
-  const path = require('path');
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default function handler(req, res) {
   let output = {};
   
   function safeReadDir(dir) {
